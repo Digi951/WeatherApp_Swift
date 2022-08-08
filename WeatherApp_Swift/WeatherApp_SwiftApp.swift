6c4fa9aff4897c273d5137f9723c3e986c25fe20
digi <Ch.Dieckmann@outlook.de>
2022-08-08 11:44:15 +0200

    Initial Commit

---
+++ WeatherApp_Swift/WeatherApp_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftApp.swift
+//  WeatherApp_Swift
+//
+//  Created by Christopher Dieckmann on 08.08.22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
